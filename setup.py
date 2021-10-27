51f902d753bfa80260a6e53f7759213d58a89fcb
Ben Gosney <bengosney@googlemail.com>
2021-10-27 21:24:38 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cam-control",
+    version="0.0.1",
+    py_modules=["cam-control"],
+    packages=find_packages(),
+    python_requires="~=3.8",
+    install_requires=[
+        "pytapo",
+        "scapy",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cam-control = main:main",
+        ],
+    },
+)
